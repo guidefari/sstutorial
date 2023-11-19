9b21eab49fd3e4b8c51ea3096fff3eaccf321f18
Guide Fari <guideg6@gmail.com>
2023-11-19 23:44:20 +0200

    feat: error handler

--- packages/frontend/src/containers/Login.tsx
+++ packages/frontend/src/containers/Login.tsx
@@ -7,6 +7,7 @@ import { Auth } from "aws-amplify";
 import { useAppContext } from "../lib/contextLib";
 import { useNavigate } from "react-router-dom";
 import LoaderButton from "../components/LoaderButton";
+import { onError } from "../lib/errorLib";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -29,11 +30,7 @@ export default function Login() {
       userHasAuthenticated(true);
       nav("/");
     } catch (error) {
-      if (error instanceof Error) {
-        alert(error.message);
-      } else {
-        alert(String(error));
-      }
+      onError(error);
       setIsLoading(false);
     }
   }
